ebce53dd1df02b01af1481fefe64f09c78381af5
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2025-12-01 09:19:44 +0530

    rotate matrix done

---
+++ lib/advanced/arrays/logical/arrays3.dart
@@ -0,0 +1,45 @@
+void main() {
+  // rotate([
+  //   [1, 2, 3],
+  //   [4, 5, 6],
+  //   [7, 8, 9]
+  // ]);
+  rotate([
+    [5, 1, 9, 11],
+    [2, 4, 8, 10],
+    [13, 3, 6, 7],
+    [15, 14, 12, 16]
+  ]);
+}
+
+// Rotate matrix by 90 degrees
+/*
+Given an N * N 2D integer matrix, rotate the matrix by 90 degrees clockwise.
+
+The rotation must be done in place, meaning the input 2D matrix must be modified directly.
+
+Input: matrix = [[1,2,3],[4,5,6],[7,8,9]]
+Output: [[7,4,1],[8,5,2],[9,6,3]]
+
+*/
+// Transpose the matrix
+// Reverse each row of the matrix
+void rotate(List<List<int>> matrix) {
+  var n = matrix.length;
+  // Transpose the matrix
+  for (var i = 0; i < n - 1; i++) {
+    for (var j = i + 1; j < n; j++) {
+      // Swap elements across the diagonal
+      var temp = matrix[i][j];
+      matrix[i][j] = matrix[j][i];
+      matrix[j][i] = temp;
+    }
+  }
+  print(matrix);
+
+  for (var i = 0; i < n; i++) {
+    matrix[i] = matrix[i].reversed.toList();
+  }
+
+  print(matrix);
+}
